78347223295a10b41695635948cbcd58ec5ca796
Chris Blake <chrisrblake93@gmail.com>
2020-02-25 13:09:25 -0600

    Part 2 missing item

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ssh interview@54.165.65.69 "git clone https://github.com/riptidewave93/test-app.git /home/interview/test-app && cd /home/interview/test-app/ && ./setup.sh && ./deploy-server.sh"
+ssh interview@54.165.65.69 "git clone https://github.com/riptidewave93/test-app.git -b improvements /home/interview/test-app && cd /home/interview/test-app/ && ./setup.sh && ./deploy-server.sh"
